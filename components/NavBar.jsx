53639a6e77c9d9446a7091f6da521081b01563ec
HamzaChbit <hamzachbit@gmail.com>
2024-01-21 19:26:39 +0100

    Add styles typewriter

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,7 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
-
+import Typewriter from 'typewriter-effect';
 const NavBar = () => {
   return (
     <nav  className='h-[65px] w-full md:py-1 py-0  shadow-navbarShadow  bg-gray-50 bottom-3   '  >  
@@ -12,7 +13,18 @@ const NavBar = () => {
 
 
             
-            <h1 className='md:text-3xl text-xl   text-black ' >  أوقات الصلاة بالمغرب</h1>
+      <h1 className='md:text-3xl text-xl   text-black ' >    
+            <Typewriter
+  options={{
+    strings: ['أوقات الصلاة بالمغرب'],
+    autoStart: true,
+    loop: true,
+  }}
+/>    
+            
+            
+            
+            </h1>
             
       
        
